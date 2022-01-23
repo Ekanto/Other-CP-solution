8fe38bc803efdb189b1e14bc214e93d0d5c8831b
Ekanto <umarekanto@gmail.com>
2022-01-23 11:53:09 +0600

    AC

---
+++ CSES Problem set/Missing number.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define ll long long
+ 
+int main(){
+    ll n, s = 0;
+    int a;
+    cin >> n;
+    for(int i = 0;i<n-1;++i){
+        cin >> a;
+        s+=a;
+    }
+    cout << n*(n+1) / 2 -s;
+    // cout << s << "\n";
+ 
+}
